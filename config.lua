f0d60f53330ea6e505d0e79cfbfacc533b00df3f
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-10-28 19:05:47 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,301 @@
+-- Combat settings
+
+-- DEBUG: show how the loader is consuming this file
+-- If your system uses globals style, the loader should NOT rely on the return value.
+-- If it does, this will make it obvious.
+
+print("[config.lua] source:", debug.getinfo(1, "S").source)
+print("[config.lua] begin load")
+
+
+print("[config.lua] begin load")
+
+-- After your weaponEvolution block (bottom of file), add:
+local function dumpType(name, val)
+  print(string.format("[config.lua] %s type = %s", name, type(val)))
+end
+
+dumpType("weaponEvolution", weaponEvolution)
+dumpType("weaponEvolution.enabled", weaponEvolution and weaponEvolution.enabled)
+dumpType("experienceStages", experienceStages)
+
+
+-- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
+worldType = "pvp"
+hotkeyAimbotEnabled = true
+protectionLevel = 1
+killsToRedSkull = 3
+killsToBlackSkull = 6
+pzLocked = 60000
+removeChargesFromRunes = true
+removeChargesFromPotions = true
+removeWeaponAmmunition = true
+removeWeaponCharges = true
+timeToDecreaseFrags = 24 * 60 * 60
+whiteSkullTime = 15 * 60
+stairJumpExhaustion = 2000
+experienceByKillingPlayers = false
+expFromPlayersLevelRange = 75
+
+-- Connection Config
+-- NOTE: maxPlayers set to 0 means no limit
+-- NOTE: allowWalkthrough is only applicable to players
+ip = "127.0.0.1"
+bindOnlyGlobalAddress = false
+loginProtocolPort = 7171
+gameProtocolPort = 7172
+statusProtocolPort = 7171
+maxPlayers = 0
+motd = "Welcome to The Forgotten Server!"
+onePlayerOnlinePerAccount = true
+allowClones = false
+allowWalkthrough = true
+serverName = "Forgotten"
+statusTimeout = 5000
+replaceKickOnLogin = true
+maxPacketsPerSecond = 25
+
+-- Pathfinding
+-- pathfindingInterval handles how often paths are force drawn
+-- pathfindingDelay delays any recently drawn paths from drawing again
+-- pathfindingDelay does not delay pathfindingInterval
+pathfindingInterval = 200
+pathfindingDelay = 300
+
+-- Deaths
+-- NOTE: Leave deathLosePercent as -1 if you want to use the default
+-- death penalty formula. For the old formula, set it to 10. For
+-- no skill/experience loss, set it to 0.
+deathLosePercent = -1
+
+-- Houses
+-- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
+-- NOTE: valid values for houseRentPeriod are: "daily", "weekly", "monthly", "yearly"
+-- use any other value to disable the rent system
+housePriceEachSQM = 1000
+houseRentPeriod = "never"
+houseOwnedByAccount = false
+houseDoorShowPrice = true
+onlyInvitedCanMoveHouseItems = true
+
+-- Item Usage
+timeBetweenActions = 200
+timeBetweenExActions = 1000
+
+-- Map
+-- NOTE: set mapName WITHOUT .otbm at the end
+mapName = "forgotten"
+mapAuthor = "Komic"
+
+-- Market
+marketOfferDuration = 30 * 24 * 60 * 60
+premiumToCreateMarketOffer = true
+checkExpiredMarketOffersEachMinutes = 60
+maxMarketOffersAtATimePerPlayer = 100
+
+-- MySQL
+mysqlHost = "75.88.83.48"
+mysqlUser = "tfs"
+mysqlPass = "ChangeThis#Now1"
+mysqlDatabase = "forgottenserver"
+mysqlPort = 3306
+mysqlSock = ""
+
+-- Misc.
+-- NOTE: classicAttackSpeed set to true makes players constantly attack at regular
+-- intervals regardless of other actions such as item (potion) use. This setting
+-- may cause high CPU usage with many players and potentially affect performance!
+-- NOTE: forceMonsterTypesOnLoad loads all monster types on startup to validate them.
+-- You can disable it to save some memory if you don't see any errors at startup.
+-- checkDuplicateStorageKeys checks the values stored in the variables for duplicates.
+allowChangeOutfit = true
+freePremium = false
+kickIdlePlayerAfterMinutes = 15
+maxMessageBuffer = 4
+emoteSpells = false
+classicEquipmentSlots = false
+classicAttackSpeed = false
+showScriptsLogInConsole = false
+showOnlineStatusInCharlist = false
+yellMinimumLevel = 2
+yellAlwaysAllowPremium = false
+minimumLevelToSendPrivate = 1
+premiumToSendPrivate = false
+forceMonsterTypesOnLoad = true
+cleanProtectionZones = false
+showPlayerLogInConsole = true
+checkDuplicateStorageKeys = false
+
+-- VIP and Depot limits
+-- NOTE: you can set custom limits per group in data/XML/groups.xml
+vipFreeLimit = 20
+vipPremiumLimit = 100
+depotFreeLimit = 2000
+depotPremiumLimit = 10000
+
+-- World Light
+-- NOTE: if defaultWorldLight is set to true the world light algorithm will
+-- be handled in the sources. set it to false to avoid conflicts if you wish
+-- to make use of the function setWorldLight(level, color)
+defaultWorldLight = true
+
+-- Server Save
+-- NOTE: serverSaveNotifyDuration in minutes
+serverSaveNotifyMessage = true
+serverSaveNotifyDuration = 5
+serverSaveCleanMap = false
+serverSaveClose = false
+serverSaveShutdown = true
+
+-- Experience stages
+-- NOTE: to use a flat experience multiplier, set experienceStages to nil
+-- minlevel and multiplier are MANDATORY
+-- maxlevel is OPTIONAL, but is considered infinite by default
+-- to disable stages, create a stage with minlevel 1 and no maxlevel
+experienceStages = {
+	{ minlevel = 1, maxlevel = 8, multiplier = 7 },
+	{ minlevel = 9, maxlevel = 20, multiplier = 6 },
+	{ minlevel = 21, maxlevel = 50, multiplier = 5 },
+	{ minlevel = 51, maxlevel = 100, multiplier = 4 },
+	{ minlevel = 101, multiplier = 3 }
+}
+
+-- Rates
+-- NOTE: rateExp is not used if you have enabled stages above
+rateExp = 5
+rateSkill = 3
+rateLoot = 2
+rateMagic = 3
+rateSpawn = 1
+
+-- Monster Despawn Config
+-- despawnRange is the amount of floors a monster can be from its spawn position
+-- despawnRadius is how many tiles away it can be from its spawn position
+-- removeOnDespawn will remove the monster if true or teleport it back to its spawn position if false
+-- walkToSpawnRadius is the allowed distance that the monster will stay away from spawn position when left with no targets, 0 to disable
+-- monsterOverspawn can be used instead of removeOnDespawn option, this will start respawn process of the monster when it goes out of deSpawn* boundaries.
+-- Setting both removeOnDespawn and monsterOverspawn to true prioritizes the latter.
+deSpawnRange = 2
+deSpawnRadius = 50
+removeOnDespawn = true
+walkToSpawnRadius = 15
+monsterOverspawn = false
+
+-- Stamina
+staminaSystem = true
+timeToRegenMinuteStamina = 3 * 60
+timeToRegenMinutePremiumStamina = 10 * 60
+
+-- Scripts
+warnUnsafeScripts = true
+convertUnsafeScripts = true
+
+-- Startup
+-- NOTE: defaultPriority only works on Windows and sets process
+-- priority, valid values are: "normal", "above-normal", "high"
+defaultPriority = "high"
+startupDatabaseOptimization = false
+
+-- Status Server Information
+ownerName = ""
+ownerEmail = ""
+url = "https://otland.net/"
+location = "Sweden"
+
+weaponEvolution = {
+        enabled = true,
+        xpPerUse = {
+                melee = 15,
+                distance = 15,
+                wand = 18,
+                shield = 12,
+                armor = 10,
+        },
+        stages = {
+                melee = {
+                        { xp = 0, attack = 0, extraDefense = 0 },
+                        { xp = 120, attack = 1 },
+                        { xp = 360, attack = 2, extraDefense = 1 },
+                        { xp = 720, attack = 3, extraDefense = 1 },
+                        { xp = 1200, attack = 4, extraDefense = 2 },
+                        { xp = 1800, attack = 5, extraDefense = 2 },
+                },
+                distance = {
+                        { xp = 0, attack = 0 },
+                        { xp = 150, attack = 1 },
+                        { xp = 420, attack = 2 },
+                        { xp = 780, attack = 3 },
+                        { xp = 1300, attack = 4 },
+                        { xp = 1900, attack = 5 },
+                },
+                wand = {
+                        { xp = 0, minMagic = 0, maxMagic = 0 },
+                        { xp = 160, minMagic = 2, maxMagic = 3 },
+                        { xp = 480, minMagic = 4, maxMagic = 6 },
+                        { xp = 920, minMagic = 6, maxMagic = 9 },
+                        { xp = 1500, minMagic = 8, maxMagic = 12 },
+                        { xp = 2200, minMagic = 10, maxMagic = 15 },
+                },
+                shield = {
+                        { xp = 0, defense = 0, extraDefense = 0 },
+                        { xp = 150, defense = 1 },
+                        { xp = 420, defense = 2, extraDefense = 1 },
+                        { xp = 780, defense = 3, extraDefense = 1 },
+                        { xp = 1300, defense = 4, extraDefense = 2 },
+                        { xp = 2000, defense = 5, extraDefense = 3 },
+                },
+                armor = {
+                        { xp = 0, armor = 0 },
+                        { xp = 200, armor = 1 },
+                        { xp = 520, armor = 2 },
+                        { xp = 940, armor = 3 },
+                        { xp = 1500, armor = 4 },
+                        { xp = 2200, armor = 5 },
+                },
+        },
+        names = {
+                melee = {
+                        "Ironborn", "Stormcleaver", "Ashrend", "Wyrmcleaver", "Bloodforged", "Thornbrand",
+                        "Emberedge", "Stonebiter", "Voidshard", "Soulpiercer", "Nightbane", "Runesteel"
+                },
+                distance = {
+                        "Ironborn", "Stormcleaver", "Ashrend", "Wyrmcleaver", "Bloodforged", "Thornbrand",
+                        "Emberedge", "Stonebiter", "Voidshard", "Soulpiercer", "Nightbane", "Runesteel"
+                },
+                shield = {
+                        "Hearthguard", "Stoneward", "Bastionplate", "Aegisborne", "Ironveil", "Skybarrier",
+                        "Bloodrest", "Frostbuckler", "Sentinel's Hold", "Riftshield", "Oakward", "Crystalbrace"
+                },
+                armor = {
+                        "Ironweld", "Gloommail", "Sunforged Hauberk", "Graveplate", "Stormwoven", "Embermail",
+                        "Lifebound Cuirass", "Runic Shell", "Boneguard", "Voidscale", "Titanhide", "Mooncase"
+                },
+                wand = {
+                        "Whisperstaff", "Manabloom Rod", "Ashwood Wand", "Arcanebone Scepter", "Starspindle", "Veilstaff",
+                        "Soulroot Rod", "Stormflame Wand", "Hexvine Staff", "Luminbar", "Riftrod", "Echo Scepter"
+                },
+                prefixes = {
+                        "Lesser", "Tempered", "Veteran", "Warded", "Charged", "Corroded",
+                        "Blessed", "Cursed", "Primal", "Ascended", "Bound", "Fragmented"
+                },
+                suffixes = {
+                        "of Echoes", "of the Fallen", "of Renewal", "of Hunger", "of the Phoenix", "of the Deep",
+                        "of Wither", "of Resonance", "of Binding", "of the Ancients", "of Endless Strikes", "of Sustenance"
+                },
+                tiers = {
+                        "Initiate", "Temper", "Warden", "Awakened", "Ascendant", "Transcendent",
+                        "Lamented", "Singular", "Eternal", "Primordial"
+                },
+                materials = {
+                        "Oaken", "Obsidian", "Starforged", "Steel-veined", "Mooniron",
+                        "Soulglass", "Embersteel", "Frostcore", "Deepstone", "Suniron"
+                },
+                corrupted = {
+                        melee = { "Blightforged", "Rotcleave", "Sundered Wyrm", "Hollowbrand", "Malignant Edge" },
+                        distance = { "Blightforged", "Rotcleave", "Sundered Wyrm", "Hollowbrand", "Malignant Edge" },
+                        shield = { "Heartrot Shield" },
+                        armor = { "Fractured Boneguard", "Dreadwoven", "Netherplate" },
+                        wand = { "Blackened Rod" }
+                }
+        }
+}
